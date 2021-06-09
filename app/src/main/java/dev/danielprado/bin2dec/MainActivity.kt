0760d8f90dab5e3ec4d054885da239de6f934515
Daniel Prado <dfe.prado@gmail.com>
2021-06-08 21:28:46 -0300

    Adiciona o ícone do aplicativo e finaliza a lógica central

--- app/src/main/java/dev/danielprado/bin2dec/MainActivity.kt
+++ app/src/main/java/dev/danielprado/bin2dec/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 import dev.danielprado.bin2dec.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,17 +14,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         layoutBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(layoutBinding.root)
-
-        layoutBinding.binaryInput.setOnKeyListener(object: View.OnKeyListener {
-            override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean {
-                if (event?.action == KeyEvent.ACTION_DOWN && keyCode != KeyEvent.KEYCODE_0 && keyCode != KeyEvent.KEYCODE_1) {
-                    Toast.makeText(applicationContext, "Only 0 and 1 allowed", Toast.LENGTH_SHORT)
-                            .show()
-                    return true
-                }
-                return false;
-            }
-        })
 
         layoutBinding.convertButton.setOnClickListener {
             convertBin2Dec()
